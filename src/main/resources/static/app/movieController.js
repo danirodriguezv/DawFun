bf4781b7754705d594775852298737d8e87500d1
Daniel Rodríguez Ventosa <d.rodriguezve@alumnos.urjc.es>
2015-05-19 01:25:39 +0200

    controlador de las pelis

--- src/main/resources/static/app/movieController.js
+++ src/main/resources/static/app/movieController.js
@@ -25,7 +25,14 @@ function movieController(movieService, $routeParams, $location, LxNotificationSe
 	
 	vm.opendDialog = function(dialogId,movie)
 	{
-		
+		vm.namemovie = movie.title;
+		vm.direct = movie.director;
+		vm.actor = movie.leading_actor;
+		vm.product = movie.production;
+		vm.dur = movie.duration;
+		vm.description = movie.description;
+		vm.trailer = movie.movie_trailer;
+		vm.movieimage = movie.photo;
 	    LxDialogService.open(dialogId);
 	};
 

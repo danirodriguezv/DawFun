7f3c2452e1d6f72157d7bd30d031630852283a05
Daniel Rodríguez Ventosa <d.rodriguezve@alumnos.urjc.es>
2015-05-19 12:22:22 +0200

    añadido locales en route

--- src/main/resources/static/app/routeconfig.js
+++ src/main/resources/static/app/routeconfig.js
@@ -11,4 +11,5 @@ function RouteConfig($routeProvider){
 	$routeProvider.when('/movies',{ templateUrl : "templates/peliculas.html"});
 	$routeProvider.when('/admin',{ templateUrl : "templates/adminAccess.html"});
 	$routeProvider.when('/promocionate',{ templateUrl : "templates/promocionate.html"});
+	$routeProvider.when('/locales',{ templateUrl : "templates/locales.html"});
 }

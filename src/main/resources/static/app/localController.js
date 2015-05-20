74f6cc0a1cc9aea7347002abca157572e8c6b012
Davidof92 <dlpez92@gmail.com>
2015-05-20 02:08:07 +0200

    locales

--- src/main/resources/static/app/localController.js
+++ src/main/resources/static/app/localController.js
@@ -9,6 +9,10 @@ function localController(localService, $routeParams, $location, LxNotificationSe
 	//View model properties
 	vm.locals = [];
 	
+	vm.objetivoBusqueda = 'Todos';
+	
+	vm.ordenadoPor = "name";
+	vm.categorias = ["PUB","RESTAURANTE"];
 		
 	//Controller logic
 	

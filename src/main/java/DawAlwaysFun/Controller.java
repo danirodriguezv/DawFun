0f8f967ca322ad25ee053bff3c93a0ae384ad71e
Davidof92 <dlpez92@gmail.com>
2015-05-21 12:53:54 +0200

    Faltan 3 pelis

--- src/main/java/DawAlwaysFun/Controller.java
+++ src/main/java/DawAlwaysFun/Controller.java
@@ -157,25 +157,25 @@ public class Controller implements CommandLineRunner {
 		
 		Movie movie = new Movie();
 		movie.setDescription("La ultima pelicula de los heroes mas heroicos, los autenticos Vengadores");
-		movie.setDirector("Michael doogleas");
-		movie.setDuration(122);
-		movie.setLeading_actor("Jason Stazam");
-		movie.setMovie_trailer("iVpF6-zMtxY"); //<------------------- AQUI
+		movie.setDirector("Joss Whedon");
+		movie.setDuration(142);
+		movie.setLeading_actor("Robert Downey, Jr.");
+		movie.setMovie_trailer("tmeOjFno6Do"); 
 		movie.setPhoto("vengadores.png");
-		movie.setProduction("WarnerBros");
-		movie.setTitle("Los vengadores, la era de ultron");
+		movie.setProduction("Marvel Studios");
+		movie.setTitle("Los vengadores: la era de ultron");
 		movie.setType("ACCION");
 		movie.setClass_type("PELICULA");
 		serviceDawFun.setMovie(movie);
 		
 		Movie movie2 = new Movie();
 		movie2.setDescription("Una historia increible sobre el señor el cual llamaron el maestro del agua");
-		movie2.setDirector("Doroti");
-		movie2.setDuration(100);
-		movie2.setLeading_actor("Rousel Crowel");
-		movie2.setMovie_trailer("De momento sin trailer");
+		movie2.setDirector("Russell Crowe");
+		movie2.setDuration(111);
+		movie2.setLeading_actor("Russell Crowe");
+		movie2.setMovie_trailer("hmgFHyDLQwc");
 		movie2.setPhoto("magua.png");
-		movie2.setProduction("WarnerBros");
+		movie2.setProduction("Warner Bros. Pictures");
 		movie2.setTitle("El maestro de agua");
 		movie2.setType("DRAMA");
 		movie2.setClass_type("PELICULA");
@@ -183,26 +183,26 @@ public class Controller implements CommandLineRunner {
 		
 		Movie movie3 = new Movie();
 		movie3.setDescription("Una precisa y emocionante carrera de coches");
-		movie3.setDirector("Gamsta");
-		movie3.setDuration(95);
-		movie3.setLeading_actor("Mulatillo");
-		movie3.setMovie_trailer("De momento sin trailer");
-		movie3.setPhoto("atodogas.png");
-		movie3.setProduction("WarnerBros");
-		movie3.setTitle("A todo gas, el mañana");
+		movie3.setDirector("James Wan");
+		movie3.setDuration(137);
+		movie3.setLeading_actor("Vin Diesel");
+		movie3.setMovie_trailer("_kOED5CCKKc");
+		movie3.setPhoto("atodogas.jpg");
+		movie3.setProduction("Universal Pictures");
+		movie3.setTitle("Fast & Furious 7");
 		movie3.setType("ACCION");
 		movie3.setClass_type("PELICULA");
 		serviceDawFun.setMovie(movie3);
 		
 		Movie movie4 = new Movie();
 		movie4.setDescription("Historia de novita y doraemon");
-		movie4.setDirector("Hulito");
-		movie4.setDuration(88);
-		movie4.setLeading_actor("PapaJoe");
-		movie4.setMovie_trailer("De momento sin trailer");
-		movie4.setPhoto("novita.png");
-		movie4.setProduction("WarnerBros");
-		movie4.setTitle("Novita y el gato cosmico");
+		movie4.setDirector("Tsutomu Shibayama");
+		movie4.setDuration(99);
+		movie4.setLeading_actor("Doraemon");
+		movie4.setMovie_trailer("hKSMTGzCMEs");
+		movie4.setPhoto("novita.gif");
+		movie4.setProduction("Toho Company");
+		movie4.setTitle("Doraemon y la fabrica de juguetes");
 		movie4.setType("COMEDIA");
 		movie4.setClass_type("PELICULA");
 		serviceDawFun.setMovie(movie4);
